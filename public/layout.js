e6dfc7dd790d903bc1b2dbc9ba76485de84f53fa
hazure03 <figmentthecat@gmail.com>
2026-07-30 19:41:33 -0500

    - New fanart in Special Killer - About section updated - Photography section added - New Link under Web Design Inspo added

--- public/layout.js
+++ public/layout.js
@@ -121,7 +121,7 @@ const headerEl = `
                 <ul class="td-side-list">
                     <li><div class="tt-content">&nbsp;<a href="/home.html">HOME</a><span class="tt-text">Where you began </span></div></li>
                     <li><div class="tt-content">&nbsp;<a href="/about.html">ABOUT</a><span class="tt-text">Who am I?</span></div></li>
-                    <li><div class="tt-content">&nbsp;<a href="/links.html">LINKS</a><span class="tt-text">Where will you go next?</span></div></li>
+                    <li><div class="tt-content">&nbsp;<a href="/links.html">LINKS</a><span class="tt-text">Where will you go next?</span><span class="blinking-text">&nbsp;NEW!</span></div></li>
                     <li><div class="tt-content">&nbsp;<a href="/index.html">SPLASH<span style="color:var(--second-text-color);">&crarr;</span></a><span class="tt-text">Where you ACTUALLY began</span></div></li>
                     <li><div class="tt-content">&nbsp;<a href="/credits.html">CREDITS</a><span class="tt-text">Credits</span></div></li>
                 </ul>
@@ -131,6 +131,7 @@ const headerEl = `
                 <ul class="td-side-list">
                     <li><div class="tt-content">&nbsp;<a href="/art.html">DIGITAL/TRAD<span style="color:var(--second-text-color);">&crarr;</span></a><span class="tt-text">*Mostly* digital drawings</span></div></li>
                     <li><div class="tt-content">&nbsp;<a href="/fursuits.html">FURSUITS<span style="color:var(--second-text-color);">&crarr;</span></a><span class="tt-text">Fursuits I've made and / or am working on</span></div></li>
+                    <li><div class="tt-content">&nbsp;<a href="/photography.html">PHOTOGRAPHY<span style="color:var(--second-text-color);">&crarr;</span></a><span class="tt-text">Photos I've taken</span><span class="blinking-text">&nbsp;NEW!</span></div></li>
                     <li><div class="tt-content">&nbsp;<a href="/TEMP.html" class="link-no-access">COMMISSIONS</a><span class="tt-text">Interested in a custom piece?</span></div></li>
                 </ul>
 
@@ -151,18 +152,18 @@ const headerEl = `
                   <li><div class="tt-content">&nbsp;<a href="/TEMP.html" class="link-no-access">VIDEOS</a><span class="tt-text">Con vids</span></div></li>
                 </ul>
 
-                <!-- OUTLAST -->
-                <p class="nav-header">MURKOFF</p>
+                <!-- SHRINES -->
+                <p class="nav-header">SHRINES</p>
                 <ul class="td-side-list">
-                  <li><div class="tt-content">&nbsp;<a href="/thesleeproom.html" target="_blank">THE_SLEEP_ROOM<span class="blinking-text">&nbsp;NEW!</span></a><span class="tt-text">Webring for The Outlast Trials</span></div></li>
-                  <li><div class="tt-content">&nbsp;<a href="/lelandcoyle.html" target="_blank">SPECIAL_KILLER<span class="blinking-text">&nbsp;NEW!</span></a><span class="tt-text"><span style="color:red;">18+</span> | Leland Coyle shrine</span></div></li>
+                  <li><div class="tt-content">&nbsp;<a href="/lelandcoyle.html" target="_blank">SPECIAL_KILLER</a><span class="tt-text"><span style="color:red;">18+</span> | Leland Coyle shrine</span><span class="blinking-text">&nbsp;NEW!</span></div></li>
+                  <li><div class="tt-content">&nbsp;<a href="*" target="_blank" class="link-no-access">SONIC_DRAGOLGO</a><span class="tt-text">TBA</span></div></li>
                 </ul>
 
                 <!--FUN-->
                 <p class="nav-header">MISC.</p>
                 <ul class="td-side-list">
+                    <li><div class="tt-content">&nbsp;<a href="/thesleeproom.html" target="_blank">THE_SLEEP_ROOM</a><span class="tt-text">Webring for The Outlast Trials</span><span class="blinking-text">&nbsp;NEW!</span></div></li>
                     <li><div class="tt-content">&nbsp;<a href="/dressup.html">DRESS.UP_GAME</a><span class="tt-text">Dress-up my fursona!</span></div></li>
-                    <!-- <li><div class="tt-content">&nbsp;<a href="/fav-characters.html">FAV_CHARACTERS</a><span class="tt-text">Current favorite characters in fiction</span></div></li> -->
                     <li><div class="tt-content">&nbsp;<a href="/web-collection.html">WEB_COLLECTION</a><span class="tt-text">Collection of cool stuff I find while browsing</span></div></li>
                     <li><div class="tt-content">&nbsp;<a href="/wallpapers.html">WALLPAPERS</a><span class="tt-text">Official wallpapers from various pieces of media</span></div></li>
                     <li><div class="tt-content">&nbsp;<a href="/archive-site.html">SITE_ARCHIVE</a><span class="tt-text">Site-related archive (old layouts, overhaul notes)</span></div></li>
